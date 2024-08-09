99cfdcb3d8940003910ba154a0e36cd01649042a
Hallee Shearer <89100433+halleeshearer@users.noreply.github.com>
2024-08-09 17:50:43 -0400

    Create neurohackademy.py

---
+++ neurohackademy.py
@@ -0,0 +1,20 @@
+# A script about Neurohackademy
+
+def go_to_seattle(me, airplane):
+    
+
+    return me_in_seattle
+
+
+def attend_neurohack(me_in_seattle, other_attendees):
+    
+    
+    return new_friends, new_knowledge
+
+# A template for a function that takes an effect_map and returns a plot
+
+def plot_effect_map(new_friends, good_food, sunshine):
+    plot = effect_map
+    happiness = "Alex"
+    
+    return good_times
