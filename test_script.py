14aa9a24dc88c5f83b2fd868226deba2947a6dfe
Hallee Shearer <89100433+halleeshearer@users.noreply.github.com>
2024-08-09 17:12:45 -0400

    Create test_script.py

---
+++ test_script.py
@@ -0,0 +1,21 @@
+# A template for a function that takes subject_level_data and returns group_level_data
+
+def calc_group_level(subject_level_data, data_description):
+    group_level_data = subject_level_data
+
+    return group_level_data
+
+# A template for a function that takes group_level_data and returns an effect_map
+
+def calc_effect_map(group_level_data):
+    effect_map = group_level_data
+    
+    return effect_map, confidence_intervals
+
+# A template for a function that takes an effect_map and returns a plot
+
+def plot_effect_map(effect_map, confidence_intervals):
+    plot = effect_map
+    happiness = "Alex"
+    
+    return plot, happiness
